d232f6c4e96e256a52201ddc899096e60962a26a
julishi <jshihadeh@scu.edu>
2018-10-29 21:32:34 -0700

    Create DatabaseTables.sql

---
+++ DatabaseTables.sql
@@ -0,0 +1,42 @@
+CREATE TABLE SCU_ALUM(
+        firstname varchar(30),
+        lastname varchar(30),
+        grad_year int,
+        PRIMARY KEY (firstname, lastname, grad_year)
+);
+
+
+CREATE TABLE Listers(
+        firstname varchar(30),
+        lastname varchar(30),
+        grad_year int,
+        businessname varchar(30),
+        approved varchar(30),
+        PRIMARY KEY (firstname, lastname, grad_year),
+        UNIQUE (businessname)
+);
+
+
+CREATE TABLE Business_Number_Email(
+        businessname varchar(30),
+        phonenumber varchar(30),
+        email varchar(30),
+        FOREIGN KEY (businessname) references Listers(businessname)
+);
+
+CREATE TABLE Business_Addresses(
+        businessname varchar(30),
+        address varchar(30),
+        city varchar(30),
+        state char(3),
+        zipcode varchar(30),
+        country varchar(30),
+        FOREIGN KEY (businessname) references Listers(businessname)
+);
+
+CREATE TABLE Business_Tags(
+        businessname varchar(30),
+        tag varchar(30),
+        FOREIGN KEY (businessname) references Listers(businessname)
+);
+                 

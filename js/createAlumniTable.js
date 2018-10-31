255b71190b2c6048e83af2e259b5c6a5927d9c0b
m-cai <36802530+m-cai@users.noreply.github.com>
2018-10-31 15:40:26 -0700

    Update createAlumniTable.js

--- js/createAlumniTable.js
+++ js/createAlumniTable.js
@@ -1,13 +1,12 @@
+/* Create the alumni table */
 function createAlumniTable(count, res) {
 
-	var checkbox = "<div class=input-group><div class=input-group-item><input type=checkbox></div></div>";
-	var list_item, item_head, item_body, item_check;
+	var checked = "<div class=input-group><div class=input-group-item><input type=checkbox checked></div></div>";
+	var not_checked = "<div class=input-group><div class=input-group-item><input type=checkbox></div></div>"
+	var list_item, item_head, item_name, item_year, item_check;
 
 	for(var i = 0; i < count; i++)
-	{
-		var obj = JSON.parse(res[i]);
-
-		
+	{	
 		list_item = document.createElement("TR");
 		list_item.id = "item" + i;
 		document.getElementById("alumni_table_body").appendChild(list_item);
@@ -19,16 +18,45 @@ function createAlumniTable(count, res) {
 		document.getElementById("item" + i).appendChild(item_head);
 
 		item_name = document.createElement("TD");
-		item_name.innerHTML = obj.name;
+		var name = res[i].FIRSTNAME + " " + res[i].LASTNAME;
+		item_name.innerHTML = name;
 		document.getElementById("item" + i).appendChild(item_name);
+
+		item_year = document.createElement("TD");
+		item_year.innerHTML = res[i].GRAD_YEAR;
+		document.getElementById("item" + i).appendChild(item_year);
 
 		item_check = document.createElement("TD");
 		item_check.id = "check" + i;
 		document.getElementById("item" + i).appendChild(item_check);
 
-		document.getElementById(item_check.id).innerHTML = checkbox;
+		if(res[i].APPROVED == "1")
+		{
+			document.getElementById(item_check.id).innerHTML = checked;
+		} else {
+			document.getElementById(item_check.id).innerHTML = not_checked;
+		}
+		
 	}
 }
 
-test = ['{ "name" : "John Doe"}', '{ "name" : "Jane Doe"}', '{ "name" : "Santa Clara" }'];
-window.onload = createAlumniTable(3, test);
+function display() {
+	var obj = { "res": "all" };
+	var dbParam = JSON.stringify(obj);
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+
+	    	//Response format: {count: #, res: []}
+	        obj = JSON.parse(xmlhttp.responseText);
+	        createAlumniTable(obj.count, obj.res);	
+	    }
+	};
+
+	xmlhttp.open("POST", "../php/getAlumniData.php", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send("x=" + dbParam);
+}
+
+window.onload = display();

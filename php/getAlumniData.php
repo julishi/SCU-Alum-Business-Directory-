6e05a552269577a530950ce07649526aca6b3f7f
m-cai <36802530+m-cai@users.noreply.github.com>
2018-10-31 15:41:07 -0700

    Add files via upload

---
+++ php/getAlumniData.php
@@ -0,0 +1,41 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$obj = json_decode($_POST["x"]);
+	
+	if($obj->res == "all") {
+		getAlumniData();
+	}
+
+}
+
+function getAlumniData() {
+
+	$conn = oci_connect('mcai', 'coen174', 'dbserver.engr.scu.edu/db11g');
+	if(!$conn) {
+		$e = oci_error();
+		print "getCardDate: connection failed:";
+		print htmlentities($e['message']);
+		exit;
+	}
+
+	$queryString = "SELECT * FROM SCU_ALUM";
+	$query = oci_parse($conn, $queryString);
+
+	$res = oci_execute($query);
+	if(!$res) {
+		$e = oci_error($query);
+		echo $e['message'];
+		exit;
+	}
+	$nrows = oci_fetch_all($query, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+	
+	$out = array('count' => $nrows, 'res' => $res);
+	echo json_encode($out);
+
+	OCILogoff($conn);
+
+}
+
+?>

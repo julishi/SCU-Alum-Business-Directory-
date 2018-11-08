0bfb16d541b5dfb82f9aa3197d5a569e08a8fb27
nkragas <nkragas@gmail.com>
2018-11-07 16:39:51 -0800

    fixed basic functionality

--- js/manageCookie.js
+++ js/manageCookie.js
@@ -1,35 +1,44 @@
 window.addEventListener("load",nameFieldInit,false);
 
+var firstName = "";
+var lastName = "";
+var gradYear = "1980";
+
 function nameFieldInit() {
-	var firstName = "";
-	var lastName = "";
-	var gradYear = "";
 	if (document.cookie != "") {
-		firstName = document.cookie.split("=")[1];
-		lastName = document.cookie.split("=")[2];
-		gradYear = document.cookie.split("=")[3];
+		firstName = document.cookie.replace(/(?:(?:^|.*;\s*)firstName\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+		lastName = document.cookie.replace(/(?:(?:^|.*;\s*)lastName\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+		gradYear = document.cookie.replace(/(?:(?:^|.*;\s*)gradYear\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 	}
 
 	document.getElementById("first-name").value = firstName;
-	document.getElementById("first-name").onblur = setCookie;
+	document.getElementById("first-name").onblur = setCookie();
 
 	document.getElementById("last-name").value = lastName;
-	document.getElementById("last-name").onblur = setCookie;
+	document.getElementById("last-name").onblur = setCookie();
 
 	//NEED TO UPDATE ELEMENT ID FOR GRADYEAR
-	document.getElementById("grad-year").value = gradYear;
-	document.getElementById("grad-year").onblur = setCookie;
+	//document.getElementById("grad-year").value = gradYear;
+	//document.getElementById("grad-year").onblur = setCookie;
 
-	document.getElementById("cookieForm").onsubmit = setCookie;
+	document.getElementById("cookieForm").onsubmit = setCookie();
 }
 
 function setCookie() {
 	var expireDate = new Date();
 	expireDate.setMonth(expireDate.getMonth()+6);
 
-	var userName = document.getElementById("first-name").value;
-	document.cookie = "firstName=" + firstName + ";lastName=" + lastName + ";gradYear=" + gradYear + ";expires=" + expireDate.toGMTString();
+	firstName = document.getElementById("first-name").value;
+	lastName = document.getElementById("last-name").value;
+	//gradYear = document.getElementById("grad-year").value;
+	document.cookie = "firstName=" + firstName;
+	document.cookie = "lastName=" + lastName;
+	document.cookie = "gradYear=" + gradYear;
 
 	document.getElementById("cookieForm").blur();
 	return false;
+}
+
+function alertCookie() {
+  alert(document.cookie);
 }

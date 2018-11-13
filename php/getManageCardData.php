5c027774e2fdf8bc0b9908a9f46faf9fea2300a3
m-cai <36802530+m-cai@users.noreply.github.com>
2018-11-12 17:18:53 -0800

    Add files via upload

---
+++ php/getManageCardData.php
@@ -0,0 +1,90 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$obj = json_decode($_POST["x"]);
+
+	if($obj->res == "new") {
+
+		getNewCardData();
+
+	} else if($obj->res == "edit") {
+
+		getEditCardData();
+
+	}
+}
+
+function console_log($data) {
+    echo '<script>';
+    echo 'console.log('. var_dump($data) .')';
+    echo '</script>';
+}
+
+function getNewCardData() {
+
+	$conn = oci_connect('mcai', 'coen174', 'dbserver.engr.scu.edu/db11g');
+	if(!$conn) {
+		$e = oci_error();
+		print "getNewCardData: connection failed:";
+		print htmlentities($e['message']);
+		exit;
+	}
+
+	$queryString = "SELECT * FROM Business_Descriptions WHERE businessname in (SELECT businessname FROM Listers WHERE approved = 0)";
+	$query = oci_parse($conn, $queryString);
+
+	$res = oci_execute($query);
+	if(!$res) {
+		$e = oci_error($query);
+		echo $e['message'];
+		exit;
+	}
+	$nrows = oci_fetch_all($query, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+	for($i = 0; $i < $nrows; $i++) {
+		if($res[$i]["IMAGE"] != null) {
+			$res[$i]["IMAGE"] = base64_encode($res[$i]["IMAGE"]);
+		}
+	}
+
+	$out = array('count' => $nrows, 'res' => $res);
+	echo json_encode($out);
+
+	oci_free_statement($query);
+	OCILogoff($conn);
+}
+
+function getEditCardData() {
+
+	$conn = oci_connect('mcai', 'coen174', 'dbserver.engr.scu.edu/db11g');
+	if(!$conn) {
+		$e = oci_error();
+		print "getEditCardData: connection failed:";
+		print htmlentities($e['message']);
+		exit;
+	}
+
+	$queryString = "SELECT * FROM Business_Descriptions WHERE businessname in (SELECT businessname FROM Business_Edits WHERE approved = 0)";
+	$query = oci_parse($conn, $queryString);
+
+	$res = oci_execute($query);
+	if(!$res) {
+		$e = oci_error($query);
+		echo $e['message'];
+		exit;
+	}
+	$nrows = oci_fetch_all($query, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+	for($i = 0; $i < $nrows; $i++) {
+		if($res[$i]["IMAGE"] != null) {
+			$res[$i]["IMAGE"] = base64_encode($res[$i]["IMAGE"]);
+		}
+	}
+
+	$out = array('count' => $nrows, 'res' => $res);
+	echo json_encode($out);
+
+	oci_free_statement($query);
+	OCILogoff($conn);
+}
+
+?>
---
+++ php/updateApproval.php
@@ -0,0 +1,49 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$obj = json_decode($_POST["x"]);
+
+	$status = $obj->status;
+	$name = $obj->businessname;
+	$type = $obj->type;
+	
+	updateApproval($status, $name, $type);
+}
+
+function console_log($data) {
+    echo '<script>';
+    echo 'console.log('. var_dump($data) .')';
+    echo '</script>';
+}
+
+function updateApproval($status, $name, $type) {
+
+	$conn = oci_connect('mcai', 'coen174', 'dbserver.engr.scu.edu/db11g');
+	if(!$conn) {
+		$e = oci_error();
+		print "updateApproval: connection failed:";
+		print htmlentities($e['message']);
+		exit;
+	}
+
+	$queryString = "begin updateApproval(:status, :name, :type); end;";
+	$query = oci_parse($conn, $queryString);
+	oci_bind_by_name($query, ':status', $status);
+	oci_bind_by_name($query, ':name', $name);
+	oci_bind_by_name($query, ':type', $type);
+
+	$res = oci_execute($query);
+	if(!$res) {
+		$e = oci_error($query);
+		echo $e['message'];
+		exit;
+	}
+
+	echo "success";
+
+	oci_free_statement($query);
+	OCILogoff($conn);
+}
+
+?>

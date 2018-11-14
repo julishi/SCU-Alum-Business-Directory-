a4c9bff69f413fb7986edbad5cf2eceea68151f0
m-cai <36802530+m-cai@users.noreply.github.com>
2018-11-13 23:55:31 -0800

    Update getManageCardData.php

--- php/getManageCardData.php
+++ php/getManageCardData.php
@@ -12,6 +12,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 		getEditCardData();
 
+	} else if($obj->res == "approved") {
+
+		getApprovedCardData();
 	}
 }
 
@@ -65,6 +68,39 @@ function getEditCardData() {
 	}
 
 	$queryString = "SELECT * FROM Business_Descriptions WHERE businessname in (SELECT businessname FROM Business_Edits WHERE approved = 0)";
+	$query = oci_parse($conn, $queryString);
+
+	$res = oci_execute($query);
+	if(!$res) {
+		$e = oci_error($query);
+		echo $e['message'];
+		exit;
+	}
+	$nrows = oci_fetch_all($query, $res, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+	for($i = 0; $i < $nrows; $i++) {
+		if($res[$i]["IMAGE"] != null) {
+			$res[$i]["IMAGE"] = base64_encode($res[$i]["IMAGE"]);
+		}
+	}
+
+	$out = array('count' => $nrows, 'res' => $res);
+	echo json_encode($out);
+
+	oci_free_statement($query);
+	OCILogoff($conn);
+}
+
+function getApprovedCardData() {
+
+	$conn = oci_connect('mcai', 'coen174', 'dbserver.engr.scu.edu/db11g');
+	if(!$conn) {
+		$e = oci_error();
+		print "getApprovedCardData: connection failed:";
+		print htmlentities($e['message']);
+		exit;
+	}
+
+	$queryString = "SELECT * FROM Business_Descriptions WHERE businessname in (SELECT businessname FROM Listers WHERE approved = 1)";
 	$query = oci_parse($conn, $queryString);
 
 	$res = oci_execute($query);

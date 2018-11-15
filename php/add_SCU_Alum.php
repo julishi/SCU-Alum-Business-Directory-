7923aa4e00041c47aca114ec3470ffedb0eaa515
m-cai <36802530+m-cai@users.noreply.github.com>
2018-11-14 17:53:41 -0800

    Update add_SCU_Alum.php

--- php/add_SCU_Alum.php
+++ php/add_SCU_Alum.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstname = $obj->firstname;
     $lastname = $obj->lastname;
     $gradyear = $obj->year;
-    
+
     if (!empty($firstname)){
         $firstname = prepareInput($firstname);
     }
@@ -28,13 +28,13 @@ function prepareInput($inputData){
 
 function insertSCUAlum($firstname, $lastname, $gradyear){
     //connect to your database. Type in your username, password and the DB path
-    $conn=oci_connect('mcai','coen174', 'dbserver.engr.scu.edu/db11g');
+    $conn=oci_connect('mcai','coen174', '//dbserver.engr.scu.edu/db11g');
     if(!$conn) {
         print "<br> connection failed:";
         exit;
     }
 
-    $query = oci_parse($conn, "Insert Into SCU_ALUM values(:firstname, :lastname, :grad_year, 1)");
+    $query = oci_parse($conn, "Insert Into SCU_ALUM values(:firstname, :lastname, :grad_year, 0)");
     oci_bind_by_name($query, ':firstname', $firstname);
     oci_bind_by_name($query, ':lastname', $lastname);
     oci_bind_by_name($query, ':grad_year', $gradyear);

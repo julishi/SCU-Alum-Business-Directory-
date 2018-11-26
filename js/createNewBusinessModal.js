7a91842e20d859dc24801ae37746dae1da05ac27
m-cai <36802530+m-cai@users.noreply.github.com>
2018-11-25 18:39:50 -0800

    Update createNewBusinessModal.js

--- js/createNewBusinessModal.js
+++ js/createNewBusinessModal.js
@@ -85,7 +85,6 @@ function createNewBusinessModal(e) {
 			var address = document.createElement("P");
 			var address_head = document.createElement("B");
 			address_head.textContent = "Address: ";
-			var address_txt2 = document.createTextNode(obj.address['CITY'] + ", " + obj.address['STATE'] + " " + obj.address['ZIPCODE']);
 			var address_txt1 = document.createElement("SPAN");
 			address_txt1.textContent = obj.address['ADDRESS'];
 			var address_txt2 = document.createElement("SPAN");

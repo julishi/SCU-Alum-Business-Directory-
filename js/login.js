fc56d27aff5f85c5945e77f3da9821f5421511d1
nkragas <44122403+nkragas@users.noreply.github.com>
2018-11-28 16:05:58 -0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -7,30 +7,28 @@ function getLoginData() {
 		window.alert("Please fill out all fields.");
 		return;
 	} else {
-    sendLoginData(user, pwd);
+		sendLoginData(user, pwd);
 	}
 }
 
 function sendLoginData(user, pwd) {
-  var obj = { "user": user, "pwd": pwd };
-  var dbParam = JSON.stringify(obj);
+	var obj = { "user": user, "pwd": pwd };
+	var dbParam = JSON.stringify(obj);
 
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-
-        obj = JSON.parse(xmlhttp.responseText);
-        if(obj.valid == 1) {
-          setLogin();
-          window.location.href = "manage_listings.html";
-        } else {
-					window.alert("Incorrect username/password. Please try again.");
-				}
-      }
-
-  };
-  xmlhttp.open("POST", "../php/login.php", true);
-  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xmlhttp.send("x=" + dbParam);
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			obj = JSON.parse(xmlhttp.responseText);
+			if(obj.valid == 1) {
+				setLogin();
+				window.location.href = "manage_listings.html";
+			} else {
+				window.alert("Incorrect username/password. Please try again.");
+			}
+		}
+	};
+	xmlhttp.open("POST", "../php/login.php", true);
+	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp.send("x=" + dbParam);
 }
 document.getElementById("submit-login").onclick = getLoginData;

14a35f2bd27b8c6d9f76c4e5278c7b76c3d39aed
m-cai <36802530+m-cai@users.noreply.github.com>
2018-12-01 21:24:33 -0800

    Update getDeleteBusinessData.php

--- php/getDeleteBusinessData.php
+++ php/getDeleteBusinessData.php
@@ -1,6 +1,10 @@
+<!-- Author: Maggie Cai -->
+<!-- File: getDeleteBusinessData.php -->
+<!-- Description: This file contains the php for getting business deletion data for a business from the database -->
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 
+// Retrieve data from the server
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $obj = json_decode($_POST["x"]);
 
@@ -10,7 +14,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     getBusinessData($name);
 }
 
-
+// Function: getBusinessData
+// Parameters:  $name string  business name
+// Returns: $out JSON JSON encoded array containing business deletion data
+// Description: This function executes a query to retrieve all data for a business from Business_Deletions
 function getBusinessData($name){
     //connect to your database. Type in your username, password and the DB path
     $conn = oci_connect('mcai', 'coen174', '//dbserver.engr.scu.edu/db11g');

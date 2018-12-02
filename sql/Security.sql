b94f35cf5f61d3b7cd64f2a7554c4ad7d8f23c49
julishi <jshihadeh@scu.edu>
2018-12-02 12:35:07 -0800

    Update Security.sql

--- sql/Security.sql
+++ sql/Security.sql
@@ -1,11 +1,14 @@
 --The code used is from https://oracle-base.com/articles/9i/storing-passwords-in-the-database-9i
 --Source code for hash function: https://asktom.oracle.com/pls/asktom/f?p=100:11:::::P11_QUESTION_ID:95412348059
--- All in pl/sql
--- Goal of this file: The file here is in charge of security procol for log-in credentials. It has a function to hash a password
--- so it's stored securly in our datatable, a function to then store this hashed password along with the person's first/last name
--- and username, functions/procedures to check if a username/password exists when someone tries to log-in, and lastly 
--- a procedure to allow a password to be reset. 
+--All in pl/sql
 
+
+--Goal of this file: The file here is in charge of security procol for log-in credentials. It has a function to hash a password
+--so it's stored securly in our datatable, a function to then store this hashed password along with the person's first/last name
+--and username, functions/procedures to check if a username/password exists when someone tries to log-in, and lastly 
+--a procedure to allow a password to be reset. 
+
+--This part of the code written by Maggie Cai
 --defining the procedures and functions 
 CREATE OR REPLACE PACKAGE user_security AS
 
@@ -33,11 +36,13 @@ END;
 /
 Show errors;
 
---body of code starts here
+--This part of the code written by Juliana Shihadeh, except for change_account_password - which was written by Maggie Cai 
+--Body of the defined functions and procedures starts here
 CREATE OR REPLACE PACKAGE BODY user_security AS
 
---to hash a password, need to take in the peron's username and password 
---technically in real life the IT desk would be in charge of inserting 
+--Function: Hashing a password
+--To hash a password, need to take in the peron's username and password. 
+--Technically if the project was to be used in real life, the IT desk would be in charge of inserting. 
   FUNCTION get_hash (p_username  IN  VARCHAR2,
                      p_password  IN  VARCHAR2)
     RETURN VARCHAR2 AS
@@ -52,8 +57,8 @@ CREATE OR REPLACE PACKAGE BODY user_security AS
                             rpad( 'X', 29, 'X' ) || 'X') );
   END;
 
--- adding a new staff
--- technically in real life the IT desk would be in charge of inserting new staff logins by running a sql command:
+-- Procedure: Adding a new staff member
+-- Again, if the project was to be used in real life the IT desk would be in charge of inserting new staff logins by running a sql command:
 -- INSERT INTO ADD_STAFF_MEMBER(.......) 
 -- Variables needed to add a new staff member: firstname, lastname, username, and password
 -- HASH function called in here to hash the password before storing it in the database
@@ -68,18 +73,21 @@ CREATE OR REPLACE PACKAGE BODY user_security AS
       password
     )
     
+    --The values that will be inserted in the table 
     VALUES (
       UPPER(v_firstname),
       UPPER(v_lastname),
       UPPER(p_username),
+      --call the hash function to hash the password before insertion
       get_hash(p_username, p_password)
     );
 
     COMMIT;
   END;
-
-  -- changing password: requires the username, old password, and new password
-  -- old password is used to look up the location of the stored data in the datatable 
+  
+  --Procedure: Change Account Password
+  --changing password: requires the username, old password, and new password
+  --old password is used to look up the location of the stored data in the datatable 
   PROCEDURE change_account_password (p_username IN VARCHAR2,
                                     p_oldpassword IN VARCHAR2,
                                     p_newpassword IN VARCHAR2) AS
@@ -104,8 +112,8 @@ CREATE OR REPLACE PACKAGE BODY user_security AS
       RAISE_APPLICATION_ERROR(-20000, 'Invalid username/password.');
   END;
 
-
-  -- searches for user, if itsn't found returns an application error 
+  --Procedure: Validata a User Account Exists
+  --searches for user, if itsn't found returns an application error 
   PROCEDURE validate_user_account_exists (p_username  IN  VARCHAR2,
                       p_password  IN  VARCHAR2) AS
   v_dummy  VARCHAR2(1);
@@ -121,6 +129,7 @@ CREATE OR REPLACE PACKAGE BODY user_security AS
       RAISE_APPLICATION_ERROR(-20000, 'Invalid username/password.');
   END;
 
+  --Function: Validate a user account exists by calling the procedure validate_user_account_exists to run the check 
   --calls the validate_user_account_exists procedure to check if user exists
   --returns 1 if a value was found, otherwise if it rain into raised error returns 0
   FUNCTION validate_user_account_exists(p_username  IN  VARCHAR2,
